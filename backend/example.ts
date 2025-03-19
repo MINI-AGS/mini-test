eb61eb7cdfea2bf9a8cc72540ee55c3d09a42616
YeyoM <yeyomoreno2003@hotmail.com>
2025-03-19 13:11:58 -0600

    fix: :fire: Change logging naming

--- backend/example.ts
+++ backend/example.ts
@@ -7,21 +7,21 @@ const example = async () => {
       name: "Ejemplo1",
       description: "Este es un ejemplo de como escribir en la base de datos",
     });
-    console.log("Document successfully written!");
+    console.log("Example: Document successfully written!");
 
     const docRef = doc(db, "ejemplos", "Ejemplo1");
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      console.log("Example: Document data:", docSnap.data());
     } else {
-      console.log("No such document!");
+      console.log("Example: No such document!");
     }
 
     await deleteDoc(docRef);
-    console.log("Document successfully deleted!");
+    console.log("Example: Document successfully deleted!");
   } catch (e) {
-    console.error("Error writing document: ", e);
+    console.error("Example: Error writing document: ", e);
   }
 };
 

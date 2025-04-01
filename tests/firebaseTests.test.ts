c65a09ae23251a014b75333742a82a81cee8d77b
YeyoM <yeyomoreno2003@hotmail.com>
2025-04-01 15:15:35 -0600

    work: :construction: Working on tests

---
+++ tests/firebaseTests.test.ts
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const firebase = require("@firebase/testing");
+
+const MY_PROJECT_ID = "mini-test-nonprod";
+
+describe("Firebase Tests", () => {
+  it("should return true", () => {
+    assert.equal(true, true);
+  });
+
+  it("Can read from the database", async () => {
+    const db = firebase
+      .initializeTestApp({
+        projectId: MY_PROJECT_ID,
+      })
+      .firestore();
+
+    const testDoc = db.collection("ejemplos").doc("testDoc");
+    await firebase.assertSucceeds(testDoc.get());
+  });
+});

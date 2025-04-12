5ce3bc301f9b53a98bb933eb6e61527b46e86c1c
YeyoM <yeyomoreno2003@hotmail.com>
2025-04-12 09:49:31 -0600

    fix: :sparkles: Add missing properties

--- tests/data/validRecord.ts
+++ tests/data/validRecord.ts
@@ -309,6 +309,8 @@ export function createValidRecordData(): Record {
     periodoE1: "No",
     diagnosticE2: "No",
     periodoE2: "No",
+    diagnosticE3: "No",
+    periodoE3: "No",
     diagnosticF1: "No",
     diagnosticF2: "No",
     diagnosticF3: "No",
